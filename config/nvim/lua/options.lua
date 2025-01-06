a6f287a915ed200b4f6d09aad76c4e4d64e3fb16
protaisM <m.protais@proton.me>
2025-01-06 09:13:09 +0100

    new nvim config

---
+++ config/nvim/lua/options.lua
@@ -0,0 +1,12 @@
+--[[
+vim.o allows you to set global vim options, but not local buffer vim options
+vim.opt has a more expansive API that can handle local and global vim options
+See :h lua-vim-options
+]]
+
+vim.o.number=true
+vim.o.relativenumber=true
+
+vim.g.leader=" "
+
+
